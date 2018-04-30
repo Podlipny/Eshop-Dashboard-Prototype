0b79ae97e6116a3475226eadfc047771a2f44b48
Podlipny <nick.47@seznam.cz>
2018-04-30 13:31:06 +0200

    packagtes upgraded to newer version - i18n is now possible

--- Eshop.Dashboard.API/ClientSource/src/app/dashboard/modules/customers/customers.module.ts
+++ Eshop.Dashboard.API/ClientSource/src/app/dashboard/modules/customers/customers.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CustomentsComponent } from './customents/customents.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [CustomentsComponent]
 })
 export class CustomersModule { }
